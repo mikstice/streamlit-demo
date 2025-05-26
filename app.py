72027d211d3346cd41c490888dd23d795e068077
J C <60640871+mikstice@users.noreply.github.com>
2025-05-26 16:04:47 -0500

    check_password_update

--- app.py
+++ app.py
@@ -5,19 +5,23 @@ import matplotlib as plt
 
 def check_password():
     def password_entered():
-        if st.session_state["password"] == "mipass123":
-            st.session_state["password_correct"] = True
+        if st.session_state.password == "mipass123":
+            st.session_state.password_correct = True
         else:
-            st.session_state["password_correct"] = False
+            st.session_state.password_correct = False
 
     if "password_correct" not in st.session_state:
-        st.text_input("Ingrese la contraseña", type="password", on_change=password_entered, key="password")
-        return False
-    elif not st.session_state["password_correct"]:
-        st.text_input("Contraseña incorrecta. Intente de nuevo", type="password", on_change=password_entered, key="password")
-        return False
-    else:
-        return True
+        st.session_state.password_correct = False
+
+    if not st.session_state.password_correct:
+        st.text_input(
+            "Ingrese contraseña:",
+            type="password",
+            key="password",
+            on_change=password_entered
+        )
+        st.stop()
+
 
 if check_password():
 
